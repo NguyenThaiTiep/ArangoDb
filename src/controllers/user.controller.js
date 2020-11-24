9da01781b82bacdc5594528a7b567ebc913b7720
NguyenTiep <nguyenthaitiep206@gmail.com>
2020-11-24 12:19:05 +0700

    update code

--- src/controllers/user.controller.js
+++
@@ -1,9 +0,0 @@
-const user = require("../models/user");
-const getAll = async (req, res) => {
-  let result = await user.find();
-  return res.send(result);
-};
-const add = async (req, res) => {
-  return res.send(200);
-};
-module.exports = { getAll, add };
--- src/router/user.router.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const userController = require("../controllers/user.controller");
-const userRouter = express.Router();
-userRouter.get("/", userController.getAll);
-module.exports = userRouter;
